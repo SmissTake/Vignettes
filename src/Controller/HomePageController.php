839551cc0fca4599cb5c85f63f606f5102b0c210
Gwion Tangi <lil.smiss7@gmail.com>
2023-01-22 21:50:09 +0100

    added homepage basics

---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\MediasRepository;
+
+class HomePageController extends AbstractController
+{
+    #[Route('/', name: 'homepage')]
+    public function index(MediasRepository $mediasRepository)
+    {
+        $medias = $mediasRepository->findBy(['status' => '1']);
+
+        //If no media found, throw an exception
+        if (!$medias) {
+            throw $this->createNotFoundException(
+                'No media found'
+            );
+        }
+
+        //Clean the array of any html tags
+        foreach ($medias as $media) {
+            $media->setTitle(strip_tags($media->getTitle()));
+            $media->setDescription(strip_tags($media->getDescription()));
+        }
+
+        return $this->render('homepage/index.html.twig', [
+            'medias' => $medias,
+        ]);
+    }
+}
