797de7c0f4c4c574cc5cb20c88969c4f6924b7a5
Gwion Tangi <lil.smiss7@gmail.com>
2023-01-23 23:33:26 +0100

    added Upload Controller (front)

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -5,14 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CategoriesRepository;
 
 class UploadController extends AbstractController
 {
     #[Route('/upload', name: 'app_upload')]
-    public function index(): Response
+    public function index(CategoriesRepository $categoriesRepository): Response
     {
+        //Get categories from database
+        $categories = $categoriesRepository->findBy(['status' => '1']);
+        
+        //If no category found, throw an exception
+        if (!$categories) {
+            throw $this->createNotFoundException(
+                'No category found'
+            );
+        }
+
         return $this->render('upload/index.html.twig', [
-            'controller_name' => 'UploadController',
+            'categories' => $categories,
         ]);
     }
 }
