fe07f73e36382d76c38326036a8a2485316be973
Gwion Tangi <lil.smiss7@gmail.com>
2023-02-01 22:15:06 +0100

    added hashed password to admin user

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -11,7 +11,7 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $admin = new Users();
-        $admin->setPassword("admin");
+        $admin->setPassword('$2y$10$LwyhDnThgFpykvf8Lwx53Oh2BJqe/A72mw6VJc.PxqFIW0Q6Bw2Bu'); # password : admin
         $admin->setEmail("admin@localhost");
         $manager->persist($admin);
 
