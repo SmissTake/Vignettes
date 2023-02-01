e3deef87b18f759aeec2c227a76e91c5e907f9fd
Lise <lise@MacBook-Air-de-Lise.local>
2023-02-01 13:54:34 +0100

    test ssh

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -12,18 +12,21 @@ class UploadController extends AbstractController
     #[Route('/upload', name: 'app_upload')]
     public function index(CategoriesRepository $categoriesRepository): Response
     {
-        //Get categories from database
-        $categories = $categoriesRepository->findBy(['status' => '1']);
+        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'User tried to access a page without having ROLE_ADMIN');
+            
+            //Get categories from database
+            $categories = $categoriesRepository->findBy(['status' => '1']);
+            
+            //If no category found, throw an exception
+            if (!$categories) {
+                throw $this->createNotFoundException(
+                    'No category found'
+                );
+            }
+    
+            return $this->render('upload/index.html.twig', [
+                'categories' => $categories,
+            ]);
         
-        //If no category found, throw an exception
-        if (!$categories) {
-            throw $this->createNotFoundException(
-                'No category found'
-            );
-        }
-
-        return $this->render('upload/index.html.twig', [
-            'categories' => $categories,
-        ]);
     }
 }
