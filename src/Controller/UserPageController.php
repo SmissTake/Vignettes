78e910fa888325c07990b9ab0b571af6f0f81cd7
Gwion Tangi <lil.smiss7@gmail.com>
2023-02-01 15:43:20 +0100

    made user page function

--- src/Controller/UserPageController.php
+++ src/Controller/UserPageController.php
@@ -5,14 +5,32 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\MediasRepository;
 
 class UserPageController extends AbstractController
 {
-    #[Route('/user/page', name: 'app_user_page')]
-    public function index(): Response
+    #[Route('/u/{id}', name: 'userpage')]
+    public function index(MediasRepository $mediasRepository, int $id)
     {
+        //Get medias from database that are active and belong to the user
+        $medias = $mediasRepository->getActiveByUser($id);
+    
+        //If no media found, throw an exception
+        if (!$medias) {
+            throw $this->createNotFoundException(
+                'No media found'
+            );
+        }
+
+        //Clean the array of any html tags
+        foreach ($medias as $media) {
+            $media->setTitle(strip_tags($media->getTitle()));
+            $media->setDescription(strip_tags($media->getDescription()));
+        }
+        
         return $this->render('user_page/index.html.twig', [
-            'controller_name' => 'UserPageController',
+            'medias' => $medias,
+            'user_id' => $id
         ]);
     }
 }
